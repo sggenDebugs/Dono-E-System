4b6745d500f4dd8baab6de83517446d2ad0eeca9
Tristan Villamil <tristanvillamil6272003@gmail.com>
2024-09-26 22:18:35 +0800

    Task#18 set up database (#19)

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,19 +1,5 @@
 <template>
-    <div>
-        <header class="shadow-sm bg-white">
-            <nav class="container mx-auto p-4 flex justify-between">
-                <ul class="flex gap-4">
-                    <li><NuxtLink to="/menuboard">Menuboard</NuxtLink></li>
-                    <li><NuxtLink to="/donoboard">Donoboard</NuxtLink></li>
-                    <li><NuxtLink to="/checkoutboard">CheckoutBoard</NuxtLink></li>
-                    <li><NuxtLink to="/signin">Signin</NuxtLink></li>
-                </ul>
-                <a href="/userprofile">UserProfile</a>
-            </nav>
-        </header> 
-        
-        <div class="container mx-auto p-1">
-            <slot />
-        </div>
+    <div class="container mx-auto p-1">
+        <slot />
     </div>
 </template>
