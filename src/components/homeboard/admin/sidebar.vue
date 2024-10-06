028fb2164dcc166117e230ce4287379031f80b38
TJVillamil <tristanvillamil6272003@gmail.com>
2024-10-06 21:10:28 +0800

    update

--- src/components/homeboard/admin/sidebar.vue
+++ src/components/homeboard/admin/sidebar.vue
@@ -1,1 +1,21 @@
-<!-- Sidebar Code -->
+<template>
+    <div class="self-stretch flex flex-col justify-center items-center text-3xl absolute inset-0">
+                
+                <div class="w-[314px] h-[75px] flex justify-center items-center text-[#f2f2f0] hover:bg-[#e4e4e4] hover:text-[#1b3c59] hover:cursor-pointer  rounded-r-[20px] font-medium font-['Ubuntu']">
+                    Homepage
+                </div>
+
+                <div class="w-[314px] h-[75px] flex justify-center items-center text-[#f2f2f0] hover:bg-[#e4e4e4] hover:text-[#1b3c59] hover:cursor-pointer  rounded-r-[20px] font-medium font-['Ubuntu']">
+                    Dono Page
+                </div>  
+
+                <div class="w-[314px] h-[75px] flex justify-center items-center text-[#f2f2f0] hover:bg-[#e4e4e4] hover:text-[#1b3c59] hover:cursor-pointer  rounded-r-[20px] font-medium font-['Ubuntu']">
+                    Checkout
+                </div>
+
+                <div class="w-[314px] h-[75px] flex justify-center items-center text-[#f2f2f0] hover:bg-[#e4e4e4] hover:text-[#1b3c59] hover:cursor-pointer  rounded-r-[20px] font-medium font-['Ubuntu']">
+                    Listings
+                </div>
+
+            </div>
+</template>
