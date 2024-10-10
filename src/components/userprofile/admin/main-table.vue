42ccd0abcfe6e3fa1fa066faeb8b71e31a8f292d
TJVillamil <tristanvillamil6272003@gmail.com>
2024-10-10 09:29:42 +0800

    update

---
+++ src/components/userprofile/admin/main-table.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-[98%] h-[fit-content] top-[140px] justify-start absolute left-3">
+      <TabView class="w-[217px] h-[37px] bg-[#f2f2f0] rounded-tl-[15px] flex-col justify-end items-center absolute left-0 top-0 ">
+        <TabPanel class="w-[216px] h-[37px] py-3 justify-center items-center gap-1 inline-flex">
+          <h1 class="text-center text-[#1b3c59] text-lg font-medium">Current Transactions</h1>
+          <div class="w-[1200px] h-[fit-content] left-0 top-[37px] absolute bg-[#f2f2f0] "></div>
+        </TabPanel>
+        <TabPanel class="w-[217px] h-[37px] py-3 bg-[#f2f2f0] justify-center items-center gap-1 inline-flex absolute rounded-tr-[15px]">
+          <h1 class="text-center text-[#1b3c59] text-lg font-medium">Transaction History</h1>
+          <div class="w-[1200px] h-[fit-content] left-0 top-[37px] absolute bg-[#f2f2f0] "></div>
+        </TabPanel>
+      </TabView>
+    </div>
+</template>
