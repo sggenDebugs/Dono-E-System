048c3a200d587802d5eaa0f76244929c3c051b27
Endorsiii <leagueofyasuo78@gmail.com>
2024-10-21 00:13:54 +0800

    initial commit

--- src/components/checkout/main-checkout.vue
+++ src/components/checkout/main-checkout.vue
@@ -1,12 +1,10 @@
 <!-- Parent File -->
  <template>
-  <div>
     <div class="font-['Ubuntu'] text-[#1B3C59] absolute left-0 top-0 mt-[1%] ml-[1.3%]">My Cart (3)</div>
-    <div class="self-stretch w-[97.4%] absolute top-[3.7%] inset-y-0 right-0 m-4">
-      <div class="w-full border border-[#D4D4D4]"></div>
+      <div class="self-stretch w-[97.4%] absolute top-[3.7%] inset-y-0 right-0 m-4">
+        <div class="w-full border border-[#D4D4D4]"></div>
+      </div>
+    <div class="font-['Ubuntu'] text-[#1B3C59] text-[1.5vw] absolute top-0 inset-x-0 flex justify-center mt-12">
+        <p>No Items Listed</p>
     </div>
-    <div class="font-['Ubuntu'] text-[#1B3C59] text-[1.5vw] absolute inset-0 flex items-center justify-center">
-      <p>No Items Listed</p>
-    </div>
-  </div>
 </template>
